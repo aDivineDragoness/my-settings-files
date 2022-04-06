f415e8142a69ef1c153cda9ee3c793a688c3f01e
Lizzy Hennig <hennig@begasoft.ch>
2022-04-06 14:37:09 +0200

    add settings based on work computer

---
+++ Shell Profiles/PWSH/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jblab_2021.omp.json | Invoke-Expression
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
